f0c1a4b5a688fc7df790e5b7202ae0fd0c877870
mladicstefan <stefan@mladicstefan.com>
2026-02-03 18:26:14 +0100

    cleanup

--- src/matrix.c
+++ src/matrix.c
@@ -1,10 +1,29 @@
-#include "matrix.h"
 #include "include/random.h"
 #include "include/synclog.h"
 #include "include/types.h"
 
 #include <stdint.h>
 #include <stdlib.h>
+
+typedef struct matrix_t
+{
+    u32 rows, cols;
+    f64 *data;
+} matrix_t;
+
+matrix_t *mat_init(u32 rows, u32 cols);
+void mat_destroy(matrix_t *m);
+void matrix_fill_rand(matrix_t *m);
+
+matrix_t *mat_add(matrix_t *a, matrix_t *b);
+matrix_t *mat_sub(matrix_t *a, matrix_t *b);
+matrix_t *mat_mult(matrix_t *a, matrix_t *b);
+void mat_scalar_mult(matrix_t *a, f64 k);
+
+matrix_t *mat_transpose(matrix_t *a);
+
+/*TODO:*/
+matrix_t *mat_inverse(matrix_t *a, matrix_t *b);
 
 static void DBG_PRINT_MATRIX(matrix_t *m)
 {
